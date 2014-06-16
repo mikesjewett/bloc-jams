1fd2d478052e26306bdf7e5671d36dafc897a47a
Mike Jewett <mikesjewett@gmail.com>
2014-06-16 10:56:53 -0400

    Added a profile and subscription page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -343,5 +343,28 @@ $(document).ready(function() {
 
 });
 
+;require.register("scripts/profile", function(exports, require, module) {
+var tabsContainer = ".user-profile-tabs-container"
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + " li").removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  console.log(selectedTabName);
+  $(".tab-pane").addClass('hidden');
+  $(selectedTabName).removeClass('hidden');
+  event.preventDefault();
+};
+
+if (document.URL.match(/\/profile.html/)) {
+  $(document).ready(function() {
+    var $tabs = $(tabsContainer + " a");
+    $tabs.click(selectTabHandler);
+    $tabs[0].click();
+  });
+}
+
+});
+
 ;
 //# sourceMappingURL=app.js.map
